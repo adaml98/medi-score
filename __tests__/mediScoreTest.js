4d5ba3b25666f703deca0cc777860f16820318d5
adaml98 <47745325+adaml98@users.noreply.github.com>
2023-10-01 21:37:55 +0100

    add test for spo2

--- __tests__/mediScoreTest.js
+++ __tests__/mediScoreTest.js
@@ -12,7 +12,7 @@ test("should return an integer", () => {
   expect(typeof result).toBe("number");
 });
 
-test("should return a score of 0 if patient does not require extra oxygen", () => {
+test("should return a score of 0 if a patient does not require extra oxygen", () => {
   const patient = {
     airOrOxygen: 0,
     consciousness: 0,
@@ -24,7 +24,7 @@ test("should return a score of 0 if patient does not require extra oxygen", () =
   expect(result).toBe(0);
 });
 
-test("should return a score of 2 if patient requires extra oxygen", () => {
+test("should return a score of 2 if a patient requires extra oxygen", () => {
   const patient = {
     airOrOxygen: 2,
     consciousness: 0,
@@ -36,7 +36,7 @@ test("should return a score of 2 if patient requires extra oxygen", () => {
   expect(result).toBe(2);
 });
 
-test("should return the correct score if patient is unconscious", () => {
+test("should return the correct score if a patient is unconscious", () => {
   const patient = {
     airOrOxygen: 2,
     consciousness: 2,
@@ -48,7 +48,7 @@ test("should return the correct score if patient is unconscious", () => {
   expect(result).toBe(4);
 });
 
-test("should return the correct score if patient's respiration rate is out of range", () => {
+test("should return the correct score if a patient's respiration rate is out of range", () => {
   const patient = {
     airOrOxygen: 2,
     consciousness: 2,
@@ -70,7 +70,7 @@ test("should return the correct score if patient's respiration rate is out of ra
   expect(result2).toBe(7);
 });
 
-test("should return the correct score if patient's temperature is abnormal", () => {
+test("should return the correct score if a patient's temperature is abnormal", () => {
   const patient = {
     airOrOxygen: 2,
     consciousness: 2,
@@ -90,4 +90,26 @@ test("should return the correct score if patient's temperature is abnormal", () 
   };
   const result2 = mediScore(patient2);
   expect(result2).toBe(5);
+});
+
+test("should return the correct score if a patients spo2 is out of range", () => {
+  const patient = {
+    airOrOxygen: 2,
+    consciousness: 0,
+    respirationRate: 15,
+    spo2: 96,
+    temperature: 37.1,
+  };
+  const result = mediScore(patient);
+  expect(result).toBe(4);
+
+  const patient2 = {
+    airOrOxygen: 0,
+    consciousness: 0,
+    respirationRate: 15,
+    spo2: 93,
+    temperature: 37.1,
+  };
+  const result2 = mediScore(patient2);
+  expect(result2).toBe(0);
 });
--- mediScore.js
+++ mediScore.js
@@ -3,28 +3,58 @@ function mediScore({
   consciousness,
   respirationRate,
   temperature,
+  spo2,
 }) {
+  let respirationScore;
+  let temperatureScore;
+  let spo2Score;
+
   if (respirationRate >= 12 && respirationRate <= 20) {
-    respirationRate = 0;
+    respirationScore = 0;
   } else if (respirationRate >= 9 && respirationRate <= 11) {
-    respirationRate = 1;
+    respirationScore = 1;
   } else if (respirationRate >= 21 && respirationRate <= 24) {
-    respirationRate = 2;
+    respirationScore = 2;
   } else {
-    respirationRate = 3;
+    respirationScore = 3;
   }
 
   if (temperature <= 35) {
-    temperature = 3;
+    temperatureScore = 3;
   } else if (temperature >= 39.1) {
-    temperature = 2;
+    temperatureScore = 2;
   } else if (temperature >= 36.1 && temperature <= 38.0) {
-    temperature = 0;
+    temperatureScore = 0;
   } else {
-    temperature = 1;
+    temperatureScore = 1;
   }
 
-  return airOrOxygen + consciousness + respirationRate + temperature;
+  if (spo2 <= 83 || (spo2 >= 97 && airOrOxygen === 2)) {
+    spo2Score = 3;
+  } else if (
+    spo2 === 84 ||
+    spo2 === 85 ||
+    (spo2 === 95 && airOrOxygen === 2) ||
+    (spo2 === 96 && airOrOxygen === 2)
+  ) {
+    spo2Score = 2;
+  } else if (
+    spo2 === 86 ||
+    spo2 === 87 ||
+    (spo2 === 93 && airOrOxygen === 2) ||
+    (spo2 === 94 && airOrOxygen === 2)
+  ) {
+    spo2Score = 1;
+  } else {
+    spo2Score = 0;
+  }
+  return (
+    airOrOxygen +
+    consciousness +
+    respirationScore +
+    temperatureScore +
+    spo2Score
+  );
 }
 
 module.exports = mediScore;
