ef3088a1229ae3e9fd4319edf8c6af68cade9074
adaml98 <47745325+adaml98@users.noreply.github.com>
2023-10-01 18:11:22 +0100

    add test for respiration rate

--- __tests__/mediScoreTest.js
+++ __tests__/mediScoreTest.js
@@ -47,3 +47,27 @@ test("should return the correct score if patient is unconscious", () => {
   const result = mediScore(patient);
   expect(result).toBe(4);
 });
+
+test("should return the correct score if patient's respiration rate is out of range", () => {
+  const patient = {
+    airOrOxygen: 2,
+    consciousness: 2,
+    respirationRate: 11,
+    spo2: 92,
+    temperature: 37.1,
+  };
+  const result = mediScore(patient);
+  expect(result).toBe(5);
+
+  const patient2 = {
+    airOrOxygen: 2,
+    consciousness: 2,
+    respirationRate: 26,
+    spo2: 92,
+    temperature: 37.1,
+  };
+  const result2 = mediScore(patient2);
+  expect(result2).toBe(7);
+});
+
+expect(result).toBe(5);
--- mediScore.js
+++ mediScore.js
@@ -1,5 +1,14 @@
-function mediScore({ airOrOxygen, consciousness }) {
-  return airOrOxygen + consciousness;
+function mediScore({ airOrOxygen, consciousness, respirationRate }) {
+  if (respirationRate >= 12 && respirationRate <= 20) {
+    respirationRate = 0;
+  } else if (respirationRate >= 9 && respirationRate <= 11) {
+    respirationRate = 1;
+  } else if (respirationRate >= 21 && respirationRate <= 24) {
+    respirationRate = 2;
+  } else {
+    respirationRate = 3;
+  }
+  return airOrOxygen + consciousness + respirationRate;
 }
 
 module.exports = mediScore;
